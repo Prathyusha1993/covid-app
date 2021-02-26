94ab3871256109ab3830b58170b6c8baa0739c8c
prathyusha1993 <pkr131023@gmail.com>
2021-02-26 11:50:36 -0800

    added constants

--- src/client/patientPortalServices/constants.js
+++ src/client/patientPortalServices/constants.js
@@ -1,5 +1,5 @@
 export const serviceConstants = {
     // HOST_NAME: 'http://3.137.173.35:6061'
-    HOST_NAME: 'https://mycovidnow.com',
-    API_HOST_NAME: 'https://mycovidnow.com/api'
+    HOST_NAME: 'https://www.mycovidnow.com',
+    API_HOST_NAME: 'https://www.mycovidnow.com/api'
 };
